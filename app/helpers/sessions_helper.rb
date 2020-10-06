03b06cb55096463df3fb80cccb29a2e1b51db78c
Nico Rithner <nicorithner@gmail.com>
2020-10-06 16:54:23 -0600

    Create Sessions_Helper
    Created sessions helper to dry up the code
    
    Co-authored-by: Nico Rithner <22056578+nicorithner@users.noreply.github.com>
    Co-authored-by: Hashim Gari <40395852+hashmaster3k@users.noreply.github.com>

---
+++ app/helpers/sessions_helper.rb
@@ -0,0 +1,20 @@
+module SessionsHelper
+  def create_session
+    if (user = User.find_by(email: params[:email]))
+      user_authenticate(user)
+    else
+      flash[:error] = 'Credentials are incorrect'
+      redirect_to '/'
+    end
+  end
+
+  def user_authenticate(user)
+    if user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to '/user/dashboard'
+    else
+      flash[:error] = 'Credentials are incorrect'
+      redirect_to '/'
+    end
+  end
+end
