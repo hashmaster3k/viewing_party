419c53c0dd7611e942b471b41b11a29b4019c114
Nico Rithner <nicorithner@gmail.com>
2020-10-06 16:57:05 -0600

    Move out 'create' code to helper
    refactor create to make it shorter. Used helper to relocate and
    refactor
    
    Co-authored-by: Nico Rithner <22056578+nicorithner@users.noreply.github.com>
    Co-authored-by: Hashim Gari <40395852+hashmaster3k@users.noreply.github.com>

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,16 +1,6 @@
 class SessionsController < ApplicationController
+  include SessionsHelper
   def create
-    if user = User.find_by(email: params[:email])
-      if user.authenticate(params[:password])
-        session[:user_id] = user.id
-        redirect_to '/user/dashboard'
-      else
-        flash[:error] = "Credentials are incorrect"
-        redirect_to '/'
-      end
-    else
-      flash[:error] = "Credentials are incorrect"
-      redirect_to '/'
-    end
+    create_session
   end
 end
