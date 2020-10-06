5a95f5bf1ac90aae104c8c334c8c90a130e2792b
Nico Rithner <nicorithner@gmail.com>
2020-10-06 08:04:08 -0600

    Create features spec for Welcome page
    Require Title, description, and presence of login and registrion links (not function yet)
    
    Co-authored-by: Nico Rithner <22056578+nicorithner@users.noreply.github.com>
    Co-authored-by: Hashim Gari <40395852+hashmaster3k@users.noreply.github.com>

---
+++ spec/features/welcome/index_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+RSpec.describe "Welcome Index Page" do
+  describe "As a visitor" do
+    it "Visit the home page and sees welcome message, button to login and registration link" do
+      visit "/"
+
+      expect(page).to have_content("Welcome to Viewing Party")
+      expect(page).to have_content("An app to watch movies with your friends")
+
+      within("#login") do
+        expect(page).to have_selector("input[value='Email']")
+        expect(page).to have_selector("input[value='Password']")
+        expect(page).to have_button("Sign In")
+      end
+
+      expect(page).to have_link("New to Viewing Party? Register Here")
+    end
+  end
+end
