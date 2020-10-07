102f6c9f4f6fb875ec2d2993348d502555e91fd6
Nico Rithner <nicorithner@gmail.com>
2020-10-07 13:41:20 -0600

    Create Friends table
    Created friends through table for follower followee relationship
    Ran migration
    
    Co-authored-by: Nico Rithner <22056578+nicorithner@users.noreply.github.com>
    Co-authored-by: Hashim Gari <40395852+hashmaster3k@users.noreply.github.com>

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_06_203135) do
+ActiveRecord::Schema.define(version: 2020_10_07_185255) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "friends", force: :cascade do |t|
+    t.integer "follower_id"
+    t.integer "followee_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email"
