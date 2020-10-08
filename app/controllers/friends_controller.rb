6e0aca30d4927d1278d58c4f7a819eafecb0b401
Hashim Gari <hashmaster3k@gmail.com>
2020-10-08 07:53:00 -0600

    Fixes RuboCop concerns

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -1,11 +1,10 @@
 class FriendsController < ApplicationController
   def create
-    if friend = User.find_by(email: params[:email])
+    if (friend = User.find_by(email: params[:email]))
       current_user.followers << friend
-      redirect_to dashboard_path
     else
       flash[:error] = "I'm sorry your friend cannot be found"
-      redirect_to dashboard_path
     end
+    redirect_to dashboard_path
   end
 end
