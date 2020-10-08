2368d4cab01a8be653e37c52b1f9aa9dccc4f98a
Hashim Gari <hashmaster3k@gmail.com>
2020-10-08 07:49:19 -0600

    Add Tests for Friend Functionality
    Co-authored-by: Nico Rithner <22056578+nicorithner@users.noreply.github.com>
    Co-authored-by: Hashim Gari <40395852+hashmaster3k@users.noreply.github.com>

--- spec/features/user/dashboard/index_spec.rb
+++ spec/features/user/dashboard/index_spec.rb
@@ -11,8 +11,61 @@ RSpec.describe "User Dashboard" do
 
       expect(page).to have_content("Welcome #{@user.humanize_username}!")
       expect(page).to have_button("Discover Movies")
-      expect(page).to have_content("Friends Section")
-      expect(page).to have_content("Viewing Parties Section")
+
+      within ".friends" do
+        expect(page).to have_content("Friends")
+      end
+
+      within ".parties" do
+        expect(page).to have_content("Viewing")
+      end
+    end
+
+    it 'display message when you have no friends' do
+      visit dashboard_path
+
+      within ".friends" do
+        expect(page).to have_content("You currently have no friends")
+      end
+    end
+
+    it 'display a box and button to add a friend' do
+      visit dashboard_path
+
+      within ".friends" do
+        expect(page).to have_selector("input[placeholder='Friends email']")
+        expect(page).to have_button('Add Friend')
+      end
+    end
+
+    it 'can add a friend and will display their name' do
+      @nick = User.create!(email: 'nick@gmail.com', password: '12345')
+
+      visit dashboard_path
+
+      within ".friends" do
+        fill_in :email, with: @nick.email
+        click_button('Add Friend')
+      end
+
+      expect(current_path).to eq(dashboard_path)
+
+      within ".friends" do
+        expect(page). to have_content('nick')
+      end
+    end
+
+    it 'cannot add a user that doesnt exit in database' do
+      @nick = User.create!(email: 'nick@gmail.com', password: '12345')
+
+      visit dashboard_path
+
+      within ".friends" do
+        fill_in :email, with: 'johnny@gmail.com'
+        click_button('Add Friend')
+      end
+
+      expect(page).to have_content("I'm sorry your friend cannot be found")
     end
   end
 end
