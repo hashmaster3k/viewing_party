6d82b1064609c9880a9fd454ea75541809d9cc0a
Hashim Gari <hashmaster3k@gmail.com>
2020-10-08 07:51:11 -0600

    Add Users to Seeds
    Co-authored-by: Nico Rithner <22056578+nicorithner@users.noreply.github.com>
    Co-authored-by: Hashim Gari <40395852+hashmaster3k@users.noreply.github.com>

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Friend.destroy_all
+User.destroy_all
+
+User.create!(email: 'fry@gmail.com', password: '123')
+User.create!(email: 'bender@gmail.com', password: '123')
+User.create!(email: 'leela@gmail.com', password: '123')
+User.create!(email: 'amy@gmail.com', password: '123')
+User.create!(email: 'hermes@gmail.com', password: '123')
+User.create!(email: 'scruffy@gmail.com', password: '123')
