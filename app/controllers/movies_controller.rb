db765bc1dedfee8a0376071db8d6363509b23903
Hashim Gari <hashmaster3k@gmail.com>
2020-10-09 14:05:36 -0600

    Add to Movie Controller to Contact API
    Co-authored-by: Nico Rithner <22056578+nicorithner@users.noreply.github.com>
    Co-authored-by: Hashim Gari <40395852+hashmaster3k@users.noreply.github.com>

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -14,4 +14,17 @@ class MoviesController < ApplicationController
       @movies = (body1[:results] << body2[:results]).flatten
     end
   end
+
+  def show
+    response = Faraday.get "https://api.themoviedb.org/3/movie/#{params[:id]}?api_key=#{ENV['TMDB_API_KEY']}&language=en-US"
+    @movie = JSON.parse(response.body, symbolize_names: true)
+
+    response = Faraday.get "https://api.themoviedb.org/3/movie/#{params[:id]}/reviews?api_key=#{ENV['TMDB_API_KEY']}&language=en-US&page=1"
+    reviews = JSON.parse(response.body, symbolize_names: true)
+    @reviews = reviews[:results]
+
+    response = Faraday.get "https://api.themoviedb.org/3/movie/#{params[:id]}/credits?api_key=#{ENV['TMDB_API_KEY']}"
+    credits = JSON.parse(response.body, symbolize_names: true)
+    @cast = credits[:cast]
+  end
 end
