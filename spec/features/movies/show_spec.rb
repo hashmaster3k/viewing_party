ec996e9dacdb3a8dab5c363bd59b985d693f39ee
Hashim Gari <hashmaster3k@gmail.com>
2020-10-09 14:03:39 -0600

    Add Initial Testing For Movie Details Page
    Co-authored-by: Nico Rithner <22056578+nicorithner@users.noreply.github.com>
    Co-authored-by: Hashim Gari <40395852+hashmaster3k@users.noreply.github.com>

--- spec/features/movies/show_spec.rb
+++ spec/features/movies/show_spec.rb
@@ -1,27 +1,21 @@
 require 'rails_helper'
 
-# RSpec.describe "Movies Show Page" do
-#   describe "As a user" do
-#     before :each do
-#       @user = User.create!(email: 'email@gmail.com', password: '12345')
-#       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
-#     end
+RSpec.describe "Movies Show Page" do
+  describe "A user" do
+    before :each do
+      @user = User.create!(email: 'email@gmail.com', password: '12345')
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
+    end
 
-#     it 'Displays selected movie' do
-#       visit movie_path
-#       find(selector) eq.('Create a Viewing Party')
-#       click_link 'Create a Viewing Party'
-#       expect(current_path).to eq('/movies')
+    it 'can click on a movie title and is taken to the movies detail page' do
+      visit discover_index_path
+      click_button('Find Top Rated Movies')
 
-#       within(".movie_container") do
-#         expect(page).to have_css(".title")
-#         name = find(".title").text
-#         expect(name).to_not be_empty
+      within(first(".movie")) do
+        click_link
+      end
 
-#         expect(page).to have_css(".rating")
-#         role = find(".rating").text
-#         expect(role).to_not be_empty
-#       end
-#     end
-#   end
-# end
+      expect(current_path).to eq("/movies/724089")
+    end
+  end
+end
