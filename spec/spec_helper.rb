7d205e7f42399a2deaa2cc6e1df36a33c56a592d
Nico Rithner <nicorithner@gmail.com>
2020-10-10 13:32:29 -0600

    Configure webmock and vcr gems
    Co-authored-by: Nico Rithner <22056578+nicorithner@users.noreply.github.com>
    Co-authored-by: Hashim Gari <40395852+hashmaster3k@users.noreply.github.com>

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -12,6 +12,8 @@
 # the additional setup, and require it from the spec files that actually need
 # it.
 #
+require 'webmock/rspec'
+
 require 'simplecov'
 SimpleCov.start 'rails' do
   add_filter '/channels/'
