bdb78f8a24796d5a87e21de630a735557f06180b
Hashim Gari <hashmaster3k@gmail.com>
2020-10-11 09:28:58 -0600

    Update test to use VCR for API call

--- spec/features/movies/index_spec.rb
+++ spec/features/movies/index_spec.rb
@@ -8,44 +8,48 @@ RSpec.describe "Movies Index Page" do
     end
 
     it 'displays top_40 movies' do
-      visit discover_index_path
-      click_button 'Find Top Rated Movies'
-      expect(current_path).to eq('/movies')
+      VCR.use_cassette('Top rated movies') do
+        visit discover_index_path
+        click_button 'Find Top Rated Movies'
+        expect(current_path).to eq('/movies')
 
-      expect(page).to have_css(".movie", count: 40)
-      within(first(".movie")) do
-        expect(page).to have_css(".title")
-        name = find(".title").text
-        expect(name).to_not be_empty
+        expect(page).to have_css(".movie", count: 40)
+        within(first(".movie")) do
+          expect(page).to have_css(".title")
+          name = find(".title").text
+          expect(name).to_not be_empty
 
-        expect(page).to have_css(".rating")
-        role = find(".rating").text
-        expect(role).to_not be_empty
+          expect(page).to have_css(".rating")
+          role = find(".rating").text
+          expect(role).to_not be_empty
+        end
       end
+
     end
 
     it "Returns movie based on search suggestion. Movie title is a link" do
-      visit discover_index_path
-      within('.search_movie_form') do
-        fill_in :title, with: 'Phoenix'
-        click_button 'Find Movies'
-      end
-      expect(current_path).to eq('/movies')
+      VCR.use_cassette('Movie Search Phoenix') do
+        visit discover_index_path
+        within('.search_movie_form') do
+          fill_in :title, with: 'Phoenix'
+          click_button 'Find Movies'
+        end
+        expect(current_path).to eq('/movies')
 
-      expect(page).to have_css(".movie", count: 40)
+        expect(page).to have_css(".movie", count: 40)
 
-      within(first(".movie")) do
-        expect(page).to have_css(".title")
-        name = find(".title").text
-        expect(page).to have_link(name)
-        expect(name.include?('Phoenix')).to eq(true)
-        expect(name).to_not be_empty
+        within(first(".movie")) do
+          expect(page).to have_css(".title")
+          name = find(".title").text
+          expect(page).to have_link(name)
+          expect(name.include?('Phoenix')).to eq(true)
+          expect(name).to_not be_empty
 
-        expect(page).to have_css(".rating")
-        role = find(".rating").text
-        expect(role).to_not be_empty
+          expect(page).to have_css(".rating")
+          role = find(".rating").text
+          expect(role).to_not be_empty
+        end
       end
     end
-
   end
 end
