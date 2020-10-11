b4e9f2a5f5337c075002b4f080b1ef09fd7ed684
Hashim Gari <hashmaster3k@gmail.com>
2020-10-11 09:29:10 -0600

    Update test to use VCR for API call

--- spec/features/movies/show_spec.rb
+++ spec/features/movies/show_spec.rb
@@ -8,60 +8,64 @@ RSpec.describe "Movies Show Page" do
     end
 
     it 'can click on a movie title and is taken to the movies detail page' do
-      visit discover_index_path
-      click_button('Find Top Rated Movies')
+      VCR.use_cassette('Top rated movies') do
+        visit discover_index_path
+        click_button('Find Top Rated Movies')
 
-      within(first(".movie")) do
-        click_link
+        within(first(".movie")) do
+          click_link
+        end
+
+        expect(current_path).to eq("/movies/724089")
       end
-
-      expect(current_path).to eq("/movies/724089")
     end
 
     it 'displays movie information' do
-      visit discover_index_path
-      click_button('Find Top Rated Movies')
+      VCR.use_cassette('Top rated movies') do
+        visit discover_index_path
+        click_button('Find Top Rated Movies')
 
-      within(first(".movie")) do
-        click_link
-      end
+        within(first(".movie")) do
+          click_link
+        end
 
-      within ".title-and-view-button" do
-        title = find(".title").text
-        expect(title).to_not be_empty
-      end
+        within ".title-and-view-button" do
+          title = find(".title").text
+          expect(title).to_not be_empty
+        end
 
-      within ".stats" do
-        vote = find(".vote").text
-        expect(vote).to_not be_empty
-      end
+        within ".stats" do
+          vote = find(".vote").text
+          expect(vote).to_not be_empty
+        end
 
-      within ".stats" do
-        runtime = find(".runtime").text
-        expect(runtime).to_not be_empty
-      end
+        within ".stats" do
+          runtime = find(".runtime").text
+          expect(runtime).to_not be_empty
+        end
 
-      within ".stats" do
-        genres = find(".genres").text
-        expect(genres).to_not be_empty
-      end
+        within ".stats" do
+          genres = find(".genres").text
+          expect(genres).to_not be_empty
+        end
 
-      within ".summary" do
-        expect(page).to have_content('Summary')
-        summary = find("p").text
-        expect(summary).to_not be_empty
-      end
+        within ".summary" do
+          expect(page).to have_content('Summary')
+          summary = find("p").text
+          expect(summary).to_not be_empty
+        end
 
-      within ".cast-list" do
-        expect(page).to have_css(".cast-person", count: 9)
-        cast = find(".cast-left").text
-        expect(cast).to_not be_empty
-        cast = find(".cast-right").text
-        expect(cast).to_not be_empty
-      end
+        within ".cast-list" do
+          expect(page).to have_css(".cast-person", count: 9)
+          cast = find(".cast-left").text
+          expect(cast).to_not be_empty
+          cast = find(".cast-right").text
+          expect(cast).to_not be_empty
+        end
 
-      within ".reviews" do
-        expect(page).to have_content('Reviews')
+        within ".reviews" do
+          expect(page).to have_content('Reviews')
+        end
       end
     end
   end
