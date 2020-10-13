1d0513411ca83629ce0f7bbd56e51e8136277b0a
Hashim Gari <hashmaster3k@gmail.com>
2020-10-12 18:22:14 -0600

    Create action functional

--- app/controllers/parties_controller.rb
+++ app/controllers/parties_controller.rb
@@ -2,6 +2,16 @@ class PartiesController < ApplicationController
   def new; end
 
   def create
+    party = Party.create(party_params)
+    params[:friends].each do |friend|
+      party.user_parties.create(host_id: current_user.id, invitee_id: friend.to_i)
+    end
     redirect_to dashboard_path
+  end
+
+  private
+
+  def party_params
+    params.permit(:movie_id, :movie_title, :duration, :date, :time, :friends)
   end
 end
