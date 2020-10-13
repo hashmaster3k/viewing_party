0e3956bfb3de6ea66404caeb9a6b5de83f3f9548
Nico Rithner <nicorithner@gmail.com>
2020-10-13 14:48:25 -0600

    Refactor movie_service file.
    Broken up the call into smaller methods to make it less repetitive
    
    Co-authored-by: Nico Rithner <22056578+nicorithner@users.noreply.github.com>
    Co-authored-by: Hashim Gari <40395852+hashmaster3k@users.noreply.github.com>

--- app/services/movie_service.rb
+++ app/services/movie_service.rb
@@ -1,17 +1,24 @@
 class MovieService
   def self.top_40
-    response1 = Faraday.get "https://api.themoviedb.org/3/movie/top_rated?api_key=#{ENV['TMDB_API_KEY']}&page=1"
-    response2 = Faraday.get "https://api.themoviedb.org/3/movie/top_rated?api_key=#{ENV['TMDB_API_KEY']}&page=2"
-    body1 = JSON.parse(response1.body, symbolize_names: true)
-    body2 = JSON.parse(response2.body, symbolize_names: true)
+    body1 = get_end_point("movie/top_rated", 1)
+    body2 = get_end_point("movie/top_rated", 2)
+    return body1, body2
+  end 
+
+  def self.search_by_title(title)
+    body1 = get_end_point("search/movie", 1, title)
+    body2 = get_end_point("search/movie", 2, title)
     return body1, body2
   end
 
-  def self.search_by_title(title)
-    response1 = Faraday.get "https://api.themoviedb.org/3/search/movie?api_key=#{ENV['TMDB_API_KEY']}&language=en-US&query=#{title}&page=1&include_adult=false"
-    response2 = Faraday.get "https://api.themoviedb.org/3/search/movie?api_key=#{ENV['TMDB_API_KEY']}&language=en-US&query=#{title}&page=2&include_adult=false"
-    body1 = JSON.parse(response1.body, symbolize_names: true)
-    body2 = JSON.parse(response2.body, symbolize_names: true)
-    return body1, body2
+  private 
+
+  def self.get_end_point(path, page, query="")
+    url = "https://api.themoviedb.org/3/#{path}?api_key=#{ENV['TMDB_API_KEY']}&page=#{page}"
+    unless query.empty?
+      url += "&query=#{query}&include_adult=false"
+    end
+    response = Faraday.get url
+    JSON.parse(response.body, symbolize_names: true)
   end
 end
