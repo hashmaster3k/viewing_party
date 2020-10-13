d716b52c08e745436c9ad38e7b52bdb65105fbc2
Nico Rithner <nicorithner@gmail.com>
2020-10-13 14:26:24 -0600

    Refactor controller to use facade pattern
    Co-authored-by: Nico Rithner <22056578+nicorithner@users.noreply.github.com>
    Co-authored-by: Hashim Gari <40395852+hashmaster3k@users.noreply.github.com>

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,17 +1,9 @@
 class MoviesController < ApplicationController
   def index
     if params[:title]
-      response1 = Faraday.get "https://api.themoviedb.org/3/search/movie?api_key=#{ENV['TMDB_API_KEY']}&language=en-US&query=#{params[:title]}&page=1&include_adult=false"
-      response2 = Faraday.get "https://api.themoviedb.org/3/search/movie?api_key=#{ENV['TMDB_API_KEY']}&language=en-US&query=#{params[:title]}&page=2&include_adult=false"
-      body1 = JSON.parse(response1.body, symbolize_names: true)
-      body2 = JSON.parse(response2.body, symbolize_names: true)
-      @movies = (body1[:results] << body2[:results]).flatten
+      @movies = MovieFacade.search_by_title(params[:title])
     else
-      response1 = Faraday.get "https://api.themoviedb.org/3/movie/top_rated?api_key=#{ENV['TMDB_API_KEY']}&page=1"
-      response2 = Faraday.get "https://api.themoviedb.org/3/movie/top_rated?api_key=#{ENV['TMDB_API_KEY']}&page=2"
-      body1 = JSON.parse(response1.body, symbolize_names: true)
-      body2 = JSON.parse(response2.body, symbolize_names: true)
-      @movies = (body1[:results] << body2[:results]).flatten
+      @movies = MovieFacade.top_40
     end
   end
 
