d9a5ccb665ad4e369bff6e77054464228cd576b9
Hashim Gari <hashmaster3k@gmail.com>
2020-10-13 16:26:19 -0600

    Update seed file to include friends and parties
    Co-authored-by: Nico Rithner 
    <22056578+nicorithner@users.noreply.github.com>
    Co-authored-by: Hashim Gari 
    <40395852+hashmaster3k@users.noreply.github.com>

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,11 +7,30 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 Friend.destroy_all
+Party.destroy_all
 User.destroy_all
 
-User.create!(email: 'fry@gmail.com', password: '123')
-User.create!(email: 'bender@gmail.com', password: '123')
-User.create!(email: 'leela@gmail.com', password: '123')
-User.create!(email: 'amy@gmail.com', password: '123')
-User.create!(email: 'hermes@gmail.com', password: '123')
-User.create!(email: 'scruffy@gmail.com', password: '123')
+fry     = User.create!(email: 'fry@gmail.com', password: '123')
+bender  = User.create!(email: 'bender@gmail.com', password: '123')
+leela   = User.create!(email: 'leela@gmail.com', password: '123')
+amy     = User.create!(email: 'amy@gmail.com', password: '123')
+hermes  = User.create!(email: 'hermes@gmail.com', password: '123')
+scruffy = User.create!(email: 'scruffy@gmail.com', password: '123')
+
+fry.followers << bender
+fry.followers << leela
+fry.followers << amy
+fry.followers << hermes
+fry.followers << scruffy
+
+leela.followers << fry
+leela.followers << bender
+leela.followers << amy
+
+party1 = Party.create!(movie_id: 1, movie_title: 'Lego Movie', duration: 180, date: "#{Date.today}", time: "#{Time.now}")
+party1.user_parties.create!(host_id: fry.id, invitee_id: leela.id)
+party1.user_parties.create!(host_id: fry.id, invitee_id: bender.id)
+
+party2 = Party.create!(movie_id: 2, movie_title: 'Batman', duration: 180, date: "#{Date.today}", time: "#{Time.now}")
+party1.user_parties.create!(host_id: leela.id, invitee_id: amy.id)
+party1.user_parties.create!(host_id: leela.id, invitee_id: hermes.id)
