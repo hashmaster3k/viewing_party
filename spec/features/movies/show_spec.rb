4fcdb844495be55575a03545443a6b189538c73a
Hashim Gari <hashmaster3k@gmail.com>
2020-10-14 13:04:55 -0600

    Fix Issue With VCR Cassettes
    Needed nested VCR statements in tests for multiple calls.
    
    Co-authored-by: Nico Rithner 
    <22056578+nicorithner@users.noreply.github.com>
    Co-authored-by: Hashim Gari 
    <40395852+hashmaster3k@users.noreply.github.com>

--- spec/features/movies/show_spec.rb
+++ spec/features/movies/show_spec.rb
@@ -8,50 +8,52 @@ RSpec.describe "Movies Show Page" do
     end
 
     it 'displays movie information' do
-      VCR.use_cassette('Movie details') do
+      VCR.use_cassette('Top rated movies') do
         visit discover_index_path
         click_button('Find Top Rated Movies')
 
-        within(first(".movie")) do
-          click_link
-        end
+        VCR.use_cassette('Movie details') do
+          within(first(".movie")) do
+            click_link
+          end
 
-        within ".title-and-view-button" do
-          title = find(".title").text
-          expect(title).to_not be_empty
-        end
+          within ".title-and-view-button" do
+            title = find(".title").text
+            expect(title).to_not be_empty
+          end
 
-        within ".stats" do
-          vote = find(".vote").text
-          expect(vote).to_not be_empty
-        end
+          within ".stats" do
+            vote = find(".vote").text
+            expect(vote).to_not be_empty
+          end
 
-        within ".stats" do
-          runtime = find(".runtime").text
-          expect(runtime).to_not be_empty
-        end
+          within ".stats" do
+            runtime = find(".runtime").text
+            expect(runtime).to_not be_empty
+          end
 
-        within ".stats" do
-          genres = find(".genres").text
-          expect(genres).to_not be_empty
-        end
+          within ".stats" do
+            genres = find(".genres").text
+            expect(genres).to_not be_empty
+          end
 
-        within ".summary" do
-          expect(page).to have_content('Summary')
-          summary = find("p").text
-          expect(summary).to_not be_empty
-        end
+          within ".summary" do
+            expect(page).to have_content('Summary')
+            summary = find("p").text
+            expect(summary).to_not be_empty
+          end
 
-        within ".cast-list" do
-          expect(page).to have_css(".cast-person", count: 9)
-          cast = find(".cast-left").text
-          expect(cast).to_not be_empty
-          cast = find(".cast-right").text
-          expect(cast).to_not be_empty
-        end
+          within ".cast-list" do
+            expect(page).to have_css(".cast-person", count: 9)
+            cast = find(".cast-left").text
+            expect(cast).to_not be_empty
+            cast = find(".cast-right").text
+            expect(cast).to_not be_empty
+          end
 
-        within ".reviews" do
-          expect(page).to have_content('Reviews')
+          within ".reviews" do
+            expect(page).to have_content('Reviews')
+          end
         end
       end
     end
--- spec/features/parties/new_spec.rb
+++ spec/features/parties/new_spec.rb
@@ -9,81 +9,91 @@ RSpec.describe "Create a new party" do
   describe "As a user" do
     describe "When the user visits the new viewing party page can create a party" do
       it "Discover movies page has a link to create a party" do
-        VCR.use_cassette( 'Movie Details' ) do
+        VCR.use_cassette( 'Top rated movies' ) do
           visit discover_index_path
           click_button "Find Top Rated Movies"
+          VCR.use_cassette('Movie_details') do
+            within(first(".movie")) do
+              click_link
+            end
 
-          within(first(".movie")) do
-            click_link
+            click_button("Create Viewing Party for Movie")
+            expect(current_path).to eq(new_party_path)
           end
-
-          click_button("Create Viewing Party for Movie")
-          expect(current_path).to eq(new_party_path)
         end
       end
 
       it "New parties page has a form to create one, with movie title, duration, date, time, and friends to invite" do
-        VCR.use_cassette( 'Movie details' ) do
+        VCR.use_cassette( 'Top rated movies' ) do
 
           visit discover_index_path
           click_button "Find Top Rated Movies"
-          within(first(".movie")) do
-            click_link
-          end
-          click_button("Create Viewing Party for Movie")
 
-          expect(page).to have_content("Movie Title")
-          expect(page).to have_content("Duration of Party")
-          expect(page).to have_content("Day")
-          expect(page).to have_content("Start Time")
-          expect(page).to have_content("Include")
-          expect(page).to have_content("You have no friends")
-          expect(page).to have_selector("input[name= 'movie_title']")
-          expect(page).to have_selector("input[name= 'duration']")
-          expect(page).to have_selector("input[name= 'date']")
-          expect(page).to have_selector("input[name= 'time']")
+          VCR.use_cassette('Movie_details') do
+            within(first(".movie")) do
+              click_link
+            end
+            click_button("Create Viewing Party for Movie")
+
+            expect(page).to have_content("Movie Title")
+            expect(page).to have_content("Duration of Party")
+            expect(page).to have_content("Day")
+            expect(page).to have_content("Start Time")
+            expect(page).to have_content("Include")
+            expect(page).to have_content("You have no friends")
+            expect(page).to have_selector("input[name= 'movie_title']")
+            expect(page).to have_selector("input[name= 'duration']")
+            expect(page).to have_selector("input[name= 'date']")
+            expect(page).to have_selector("input[name= 'time']")
+          end
         end
       end
 
       it 'can create a new viewing party with friends' do
-        VCR.use_cassette( 'Movie details' ) do
+        VCR.use_cassette( 'Top Rated Movies' ) do
           @bob = User.create!(email: 'bob@gmail.com', password: '12345')
           @joe.followers << @bob
 
           visit discover_index_path
           click_button "Find Top Rated Movies"
-          within(first(".movie")) do
-            click_link
-          end
-          click_button("Create Viewing Party for Movie")
-          fill_in :duration, with: 115
-          fill_in :date, with: Date.today
-          fill_in :time, with: Time.now
-          within "#friend-#{@bob.id}" do
-            check
-          end
 
-          click_button("Create Party")
-          expect(current_path).to eq(dashboard_path)
+          VCR.use_cassette('Movie details') do
+            within(first(".movie")) do
+              click_link
+            end
+            click_button("Create Viewing Party for Movie")
+            fill_in :duration, with: 115
+            fill_in :date, with: Date.today
+            fill_in :time, with: Time.now
+            within "#friend-#{@bob.id}" do
+              check
+            end
+
+            click_button("Create Party")
+            expect(current_path).to eq(dashboard_path)
+          end
         end
       end
 
       it 'can create a new viewing party with no friends' do
-        VCR.use_cassette( 'Movie details' ) do
+        VCR.use_cassette( 'Top rated movies' ) do
           @bob = User.create!(email: 'bob@gmail.com', password: '12345')
 
           visit discover_index_path
           click_button "Find Top Rated Movies"
-          within(first(".movie")) do
-            click_link
-          end
-          click_button("Create Viewing Party for Movie")
-          fill_in :duration, with: 115
-          fill_in :date, with: Date.today
-          fill_in :time, with: Time.now
 
-          click_button("Create Party")
-          expect(current_path).to eq(dashboard_path)
+          VCR.use_cassette('Movie details') do
+            within(first(".movie")) do
+              click_link
+            end
+            click_button("Create Viewing Party for Movie")
+            fill_in :duration, with: 115
+            fill_in :date, with: Date.today
+            fill_in :time, with: Time.now
+
+            click_button("Create Party")
+            expect(current_path).to eq(dashboard_path)
+          end
         end
       end
     end
